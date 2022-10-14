6ae31c0a13c62f96fa93e8c63ce4b263bec7ab81
Ngazetungue <nmuheuez@gmail.com>
2022-10-14 18:35:41 +0200

    update views

--- farmer/views.py
+++ farmer/views.py
@@ -13,28 +13,30 @@ from django.urls import reverse_lazy
 # Create your views here.
 
 
-class HomePageView(TemplateView):
+class HomePageView(LoginRequiredMixin, TemplateView):
     template_name = "farmer/index.html"
+
 
 class FarmerApplyCreateView(CreateView):
     model = Client
     template_name = "farmer/pages/farmer_apply_create.html"
     success_url = reverse_lazy("login")
     fields = [
-        'first_name',
-'last_name',
-'email',
-'town',
-'number_of_livestock',
-    
+        "first_name",
+        "last_name",
+        "email",
+        "town",
+        "number_of_livestock",
     ]
 
-class FarmerListView(ListView):
+
+class FarmerListView(LoginRequiredMixin, ListView):
     model = Client
     context_object_name = "farmer_list"
     template_name = "farmer/pages/tables.html"
 
-class BillingCreateView(CreateView):
+
+class BillingCreateView(LoginRequiredMixin, CreateView):
     model = Billing
     template_name = "farmer/pages/billing_create.html"
     success_url = reverse_lazy("billing")
@@ -44,19 +46,20 @@ class BillingCreateView(CreateView):
         "invoice_status",
     ]
 
-class BillingListView(ListView):
+
+class BillingListView(LoginRequiredMixin, ListView):
     model = Billing
     context_object_name = "billing_list"
     template_name = "farmer/pages/billing.html"
 
 
-class BillingDetailView(DetailView):
+class BillingDetailView(LoginRequiredMixin, DetailView):
     model = Billing
     context_object_name = "billing_detail"
     template_name = "formaer/pages/billing_detail.html"
 
 
-class BillingUpdateView(UpdateView):
+class BillingUpdateView(LoginRequiredMixin, UpdateView):
     model = Billing
     template_name = "farmer/pages/billing_update.html"
     success_url = reverse_lazy("billing")
@@ -67,7 +70,7 @@ class BillingUpdateView(UpdateView):
     ]
 
 
-class BillingDeleteView(DeleteView):
+class BillingDeleteView(LoginRequiredMixin, DeleteView):
     model = Billing
     template_name = "farmer/pages/billing_delete.html"
     success_url = reverse_lazy("billing")

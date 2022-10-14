f7795cf3686b9ecfe822835fea055a107d8811ad
Ngazetungue <nmuheuez@gmail.com>
2022-10-14 18:53:20 +0200

    testing views and urls

--- farmer/tests.py
+++ farmer/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+def test_view_url_by_name(self):
+    response = self.client.get(reverse("home"))
+    self.assertEqual(response.status_code, 200)
+
+
+def test_view_uses_correct_template(self):
+    response = self.client.get(reverse("home"))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, "home.html")

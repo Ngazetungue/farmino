8806a409e4b6404581d3f2641c0a11ae047e72f0
Ngazetungue <nmuheuez@gmail.com>
2022-10-14 19:48:52 +0200

    Update tests.py

--- farmer/tests.py
+++ farmer/tests.py
@@ -1,8 +1,15 @@
-
-from django.test import SimpleTestCase, TestCase
 from django.urls import reverse
+
+from django.test import TestCase
 
 
 def test_home_page_status_code(self):
-        response = self.client.get('/home/')
-        self.assertEquals(response.status_code, 200)
+    response = self.client.get("/home/")
+    self.assertEquals(response.status_code, 200)
+
+
+def test_farmer_list_view(self):
+    response = self.client.get(reverse("farmer-list"))
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, "Ziddan")
+    self.assertTemplateUsed(response, "farmer/pages/tables.html")

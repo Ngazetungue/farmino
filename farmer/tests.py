014cdf9b1cc9f92d31b3e0c25dee03a05b670a77
Ngazetungue <nmuheuez@gmail.com>
2022-10-14 18:57:28 +0200

    Update tests.py

--- farmer/tests.py
+++ farmer/tests.py
@@ -9,12 +9,12 @@ class HomePageTests(SimpleTestCase):
         self.assertEqual(response.status_code, 200)
 
 
-def test_view_url_by_name(self):
-    response = self.client.get(reverse("home"))
-    self.assertEqual(response.status_code, 200)
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
 
 
-def test_view_uses_correct_template(self):
-    response = self.client.get(reverse("home"))
-    self.assertEqual(response.status_code, 200)
-    self.assertTemplateUsed(response, "home.html")
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
